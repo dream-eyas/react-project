7d19223838d74cb626a5a41f991ca365db15c031
eyas <584800573@qq.com>
2020-02-03 01:25:55 +0800

    新需求

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,21 +1,6 @@
-const defaultState = {
-    focused: false
-};
+import { combineReducers } from 'redux';
+import headerReducer from '../common/header/store/reducer';
 
-export default (state = defaultState, action) => {
-    console.log(action);
-    console.log(action.type);
-    const type = action.type;
-    if (type === 'search_focus'){
-        const newState = JSON.parse(JSON.stringify(state));
-        // 修改newState数据并返回
-        newState.focused = true;
-        return newState;
-    } else if (type === 'search_blur') {
-        const newState = JSON.parse(JSON.stringify(state));
-        // 修改newState数据并返回
-        newState.focused = false;
-        return newState;
-    }
-    return state;
-}
+export default combineReducers({
+    header: headerReducer
+})
